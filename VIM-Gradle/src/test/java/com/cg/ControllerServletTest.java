3f68c7c1a085893ab2793f1c3f9ff2801d366a6c
JitendraKhairnar28 <jitendra.khairnar@capgemini.com>
2017-08-21 19:25:14 -1200

    Update ControllerServletTest.java

--- VIM-Gradle/src/test/java/com/cg/ControllerServletTest.java
+++ VIM-Gradle/src/test/java/com/cg/ControllerServletTest.java
@@ -2,10 +2,11 @@ package com.cg;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
 
 public class ControllerServletTest {
 
+//	@Test
 	@Ignore
 	public void testProcessRequest() {
 		fail("Not yet implemented");
